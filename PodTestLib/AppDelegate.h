c749a353391ae65c716d3977d2cccfbae34021aa
Osamushi <mad.naname@gmail.com>
2015-08-31 20:31:32 +0900

    Initial Commit

---
+++ PodTestLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTestLib
+//
+//  Created by 加藤直人 on 8/31/15.
+//  Copyright (c) 2015 加藤直人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTestLib/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PodTestLib
+//
+//  Created by 加藤直人 on 8/31/15.
+//  Copyright (c) 2015 加藤直人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
